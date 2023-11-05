23788d2d15509511734c58f4793510ff4a713ce6
Candace Hendricks <candiiihendricks@gmail.com>
2023-11-05 16:43:04 -0500

    Added the tic-tac-toe.js

---
+++ tic-tac-toe.js
@@ -0,0 +1,38 @@
+window.onload = function(){
+
+
+const parentdiv = document.getElementById("board");
+const childdiv  = parentdiv.getElementsByTagName("div");
+
+for (let i = 0; i < childdiv.length; i++){
+      childdiv[i].classList.add("square")
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
