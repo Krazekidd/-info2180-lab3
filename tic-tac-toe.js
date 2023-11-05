1d94c9803a53fde1bf33ed68386752f1710dd9f0
Candace Hendricks <candiiihendricks@gmail.com>
2023-11-05 17:31:35 -0500

    Added an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,9 +7,32 @@ const childdiv  = parentdiv.getElementsByTagName("div");
 for (let i = 0; i < childdiv.length; i++){
       childdiv[i].classList.add("square")
 }
+ 
+let y = [null, null, null,null, null, null,null, null, null];
+let player = "X"; 
 
+for ( const div of childdiv){
+    div.addEventListener("click", function(e) {
+        const i = Array.from (childdiv).indexOf(div);
+        if (y[i] == null){
+         div.textContent = player;
+         y[i] = player;
+         div.classList.add(player);
+        if (player =='X') {
+        div.classList.add("square.X");
+        }
+        else if (player == 'O') {
+        div.classList.add(".square.O");
+        }
+        player = (player === 'X') ? 'O' : 'X';
+
+        }
+    })
+}
 
 }
+
+
 
 
 
