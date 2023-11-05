f61f18b8de8f51bce0cf83c842578ca8e9e1220b
Candace Hendricks <candiiihendricks@gmail.com>
2023-11-05 18:01:57 -0500

    Change the style when you move your mouse over a square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,26 +11,44 @@ for (let i = 0; i < childdiv.length; i++){
 let y = [null, null, null,null, null, null,null, null, null];
 let player = "X"; 
 
+
+
 for ( const div of childdiv){
-    div.addEventListener("click", function(e) {
+    div.addEventListener("click", function(e){
         const i = Array.from (childdiv).indexOf(div);
         if (y[i] == null){
          div.textContent = player;
          y[i] = player;
          div.classList.add(player);
         if (player =='X') {
-        div.classList.add("square.X");
+         div.classList.add("square.X");  
         }
         else if (player == 'O') {
-        div.classList.add(".square.O");
+         div.classList.add(".square.O");  
         }
         player = (player === 'X') ? 'O' : 'X';
-
         }
+    });
+    div.addEventListener('mouseover', function(){
+        div.classList.add('hover');
+    })
+
+    div.addEventListener('mouseout', function(e){
+        div.classList.remove('hover')
     })
 }
+    
 
+
+
+    
 }
+    
+
+
+
+
+
 
 
 
