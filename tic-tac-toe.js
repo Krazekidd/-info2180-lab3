62564d1c843e4e805c18fb54ea78e200a1a467be
Candace Hendricks <candiiihendricks@gmail.com>
2023-12-05 20:03:30 -0500

    Edited

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,79 +1,88 @@
-window.onload = function(){
+// add square to divs to make game grid and add event listener to squares
+window.onload = function() {
+    var squares = document.getElementById("board").children;
+    for (i = 0; i <= squares.length - 1; i++) {
+        squares[i].classList.add("square"); // add class square
+        squares[i].addEventListener("click", makeMove, false); // add click even listener
+        squares[i].addEventListener("mouseover", squareHover, false);
+        squares[i].addEventListener("mouseout", squareExit, false);  
+        squares[i].setAttribute('id', i); // add index loop as id to squares
+    }
+    // click event for new game button
+    document.querySelector("button").addEventListener("click", restartGame);
+};
+ var slots = [[0,1,2], [3,4,5], [6,7,8],
+ [0,3,6], [1,4,7], [2,5,8],
+ [0,4,8], [2,4,6]];
 
-
-const parentdiv = document.getElementById("board");
-const childdiv  = parentdiv.getElementsByTagName("div");
-
-for (let i = 0; i < childdiv.length; i++){
-      childdiv[i].classList.add("square")
-}
- 
-let y = [null, null, null,null, null, null,null, null, null];
-let player = "X"; 
-
-
-
-for ( const div of childdiv){
-    div.addEventListener("click", function(e){
-        const i = Array.from (childdiv).indexOf(div);
-        if (y[i] == null){
-         div.textContent = player;
-         y[i] = player;
-         div.classList.add(player);
-        if (player =='X') {
-         div.classList.add("square.X");  
+// When a square is clicked add X or O to it
+var prevMove = null;
+function  makeMove(e) {
+    // Check if the square is empty to Disallow Cheating 
+    if (e.target.innerHTML == "") {
+        // first move of the game
+        if (prevMove == null) {
+            e.target.innerHTML = "X";
+            e.target.classList.add("X");
+            prevMove = 1;        
+        }else{
+            // alternate bettween 'X' and 'O'
+            if (prevMove == 1) {
+                e.target.innerHTML = "O";
+                e.target.classList.add("O");
+                prevMove = 0;
+            }
+            else if (prevMove == 0) {
+                e.target.innerHTML = "X";
+                e.target.classList.add("X");
+                prevMove = 1;
+            }
         }
-        else if (player == 'O') {
-         div.classList.add(".square.O");  
-        }
-        player = (player === 'X') ? 'O' : 'X';
-        }
-    });
-    div.addEventListener('mouseover', function(){
-        div.classList.add('hover');
-    })
-
-    div.addEventListener('mouseout', function(e){
-        div.classList.remove('hover')
-    })
+    }
+    // after each move check for a winner
+    winner()
 }
-    
 
 
-
-    
+function squareHover(e) {
+    e.target.classList.add("hover");
 }
-    
+function squareExit(e) {
+    e.target.classList.remove("hover");
+}
 
 
+function winner() {
+    var squares = document.getElementById("board").children;
+    for ( i=0; i<slots.length; i++ ) {
+
+        if (squares[slots[i][0]].innerHTML != "" || squares[slots[i][1]].innerHTML != "" || squares[slots[i][2]].innerHTML != "" ) {
+            if ( squares[slots[i][0]].innerHTML  == squares[slots[i][1]].innerHTML && squares[slots[i][1]].innerHTML == squares[slots[i][2]].innerHTML ) {
+                whoWon(squares[slots[i][0]].innerHTML);
+            }
+        }
+    }
+}
+
+function whoWon(player){
+    var status = document.getElementById("status");
+    if (player == "X") {
+        status.innerHTML = "Congratulations! 'X' is the Winner!";
+    }
+    else if(player == "O"){
+        status.innerHTML = "Congratulations! 'O' is the Winner!";
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function restartGame() {
+    var status = document.getElementById("status");
+    var squares = document.getElementById("board").children;
+    for (i = 0; i <= squares.length - 1; i++) {
+        squares[i].classList.remove("X"); 
+        squares[i].classList.remove("O"); 
+        squares[i].innerHTML = ""; 
+    }
+    status.innerHTML = "Move your mouse over a square and click to play an X or an O.";
+    prevMove = null;
+}
